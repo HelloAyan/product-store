62aa3651bbf8376d85905bc7eb470cf6015a587f
HelloAyan <18103373@iubat.edu>
2023-11-18 22:23:55 +0600

    modified

---
+++ resources/views/add.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <style>
+    <style>
+
+    /* Styling for the form */
+    body {
+        font-family: Arial, sans-serif;
+    }
+
+    form {
+        width: 50%;
+        margin: 20px auto;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 5px;
+    }
+
+    input[type="text"],
+    input[type="number"] {
+        width: calc(100% - 10px);
+        padding: 5px;
+        margin-bottom: 10px;
+    }
+
+    input[type="submit"] {
+        padding: 8px 20px;
+        background-color: #4CAF50;
+        color: white;
+        border: none;
+        cursor: pointer;
+    }
+
+    /* Error message styling */
+    .error {
+        color: red;
+        margin-top: 5px;
+    }
+    </style>
+    </style>
+
+</body>
+
+</html>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,64 +6,66 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/style.css" rel="stylesheet">
     <title>Product Details</title>
+
+    <style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .container {
+        width: 100%;
+        height: 40px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #e6e4e4;
+    }
+
+    .title {
+        font-size: 30px;
+        color: green;
+    }
+
+    .title a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    .table_section {
+        width: 80%;
+        height: auto;
+        padding-top: 100px;
+        margin: auto;
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    th,
+    td {
+        border: 1px solid #dddddd;
+        padding: 8px;
+        text-align: left;
+    }
+
+    tr:nth-child(odd) {
+        background-color: white;
+    }
+
+    tr:nth-child(even) {
+        background-color: lightgray;
+    }
+
+    th {
+        background-color: #f2f2f2;
+    }
+    </style>
 </head>
-<style>
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
 
-.container {
-    width: 100%;
-    height: 40px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #e6e4e4;
-}
-
-.title {
-    font-size: 30px;
-    color: green;
-}
-
-.title a {
-    text-decoration: none;
-    color: inherit;
-}
-
-.table_section {
-    width: 80%;
-    height: auto;
-    padding-top: 100px;
-    margin: auto;
-}
-
-table {
-    width: 100%;
-    border-collapse: collapse;
-}
-
-th,
-td {
-    border: 1px solid #dddddd;
-    padding: 8px;
-    text-align: left;
-}
-
-tr:nth-child(odd) {
-    background-color: white;
-}
-
-tr:nth-child(even) {
-    background-color: lightgray;
-}
-
-th {
-    background-color: #f2f2f2;
-}
-</style>
 
 <body>
     <div class='container'>
