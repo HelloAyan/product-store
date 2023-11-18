eb4cbf7f973bce2cb662522d44da794489439f1c
HelloAyan <18103373@iubat.edu>
2023-11-18 22:27:02 +0600

    add page design added

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -5,12 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
 
-<body>
     <style>
-    <style>
-
     /* Styling for the form */
     body {
         font-family: Arial, sans-serif;
@@ -47,8 +43,105 @@
         margin-top: 5px;
     }
     </style>
-    </style>
+</head>
 
+<body>
+    <form id="myForm" onsubmit="return validateForm()">
+        <label for="id">ID:</label>
+        <input type="number" id="id" name="id" required>
+        <span class="error" id="idError"></span>
+
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+        <span class="error" id="nameError"></span>
+
+        <label for="qty">Qty:</label>
+        <input type="number" id="qty" name="qty" required>
+        <span class="error" id="qtyError"></span>
+
+        <label for="price">Price:</label>
+        <input type="number" id="price" name="price" required>
+        <span class="error" id="priceError"></span>
+
+        <label for="brand">Brand:</label>
+        <input type="text" id="brand" name="brand" required>
+        <span class="error" id="brandError"></span>
+
+        <label for="color">Color:</label>
+        <input type="text" id="color" name="color" required>
+        <span class="error" id="colorError"></span>
+
+        <label for="details">Details:</label>
+        <input type="text" id="details" name="details" required>
+        <span class="error" id="detailsError"></span>
+
+        <input type="submit" value="Submit">
+    </form>
+
+
+    <script>
+    function validateForm() {
+        let isValid = true;
+
+        const id = document.getElementById('id').value.trim();
+        if (id === '') {
+            document.getElementById('idError').innerText = 'Please enter ID';
+            isValid = false;
+        } else {
+            document.getElementById('idError').innerText = '';
+        }
+
+        const name = document.getElementById('name').value.trim();
+        if (name === '') {
+            document.getElementById('nameError').innerText = 'Please enter Name';
+            isValid = false;
+        } else {
+            document.getElementById('nameError').innerText = '';
+        }
+
+        const qty = document.getElementById('qty').value.trim();
+        if (qty === '') {
+            document.getElementById('qtyError').innerText = 'Please enter Qty';
+            isValid = false;
+        } else {
+            document.getElementById('qtyError').innerText = '';
+        }
+
+        const price = document.getElementById('price').value.trim();
+        if (price === '') {
+            document.getElementById('priceError').innerText = 'Please enter Price';
+            isValid = false;
+        } else {
+            document.getElementById('priceError').innerText = '';
+        }
+
+        const brand = document.getElementById('brand').value.trim();
+        if (brand === '') {
+            document.getElementById('brandError').innerText = 'Please enter Brand';
+            isValid = false;
+        } else {
+            document.getElementById('brandError').innerText = '';
+        }
+
+        const color = document.getElementById('color').value.trim();
+        if (color === '') {
+            document.getElementById('colorError').innerText = 'Please enter Color';
+            isValid = false;
+        } else {
+            document.getElementById('colorError').innerText = '';
+        }
+
+        const details = document.getElementById('details').value.trim();
+        if (details === '') {
+            document.getElementById('detailsError').innerText = 'Please enter Details';
+            isValid = false;
+        } else {
+            document.getElementById('detailsError').innerText = '';
+        }
+
+        return isValid;
+    }
+    </script>
 </body>
 
 </html>
